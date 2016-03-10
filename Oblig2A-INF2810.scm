9dba8004f7bfb401d11d18c1f5d7b7834492d9ec
axelbaxel <axelbaxel@gmail.com>
2016-03-10 11:22:40 +0100

    Update Oblig2A-INF2810.scm

--- Oblig2A-INF2810.scm
+++ Oblig2A-INF2810.scm
@@ -6,12 +6,19 @@
 (define (p-cons x y)
   (lambda (proc) (proc x y)))
 
-(define (p-car (proc x y))
-  (x))
-(define (p-cdr (proc x y))
-  (y))
-;;(p-cons "foo" "bar")
+(define (p-car proc)
+  (proc (lambda (x y) x)))
+(define (p-cdr proc)
+  (proc (lambda (x y) y)))
 
-;;(p-car (p-cons "foo" "bar"))
 
-(p-cdr (p-cons "foo" "bar"))
+;;Oppgave 2
+;;a)
+
+(define (member? symb items)
+  (define (iter rest)
+    (cond ((null? rest) #f)
+          ((eq? (car rest) symb) #t)
+          (else (iter (cdr rest)))))
+  (iter items))
+          
