7aa79f66ccf448502f450e458aeb74f2893fc4ba
axelbaxel <axelbaxel@gmail.com>
2016-03-10 12:04:30 +0100

    Siste versjon

--- Oblig2A-INF2810.scm
+++ Oblig2A-INF2810.scm
@@ -11,6 +11,34 @@
 (define (p-cdr proc)
   (proc (lambda (x y) y)))
 
+;;b)
+(define foo 30)
+
+(let ((x foo)
+      (y 20))
+  (+ foo x y))
+
+((lambda (x y)
+   (+ foo x y))
+foo 20)
+
+(let ((foo 10))
+  (let ((x foo)
+        (y 20))
+    (+ foo x y)))
+
+((lambda (foo)
+    ((lambda (x y)
+       (+ foo x y))
+      foo 20))
+    10)
+
+;;c)
+(define a1 (list 1 2 3 4))
+(define a2 (list + - * /))
+(define a3 (list 5 6 7 8))
+(map (lambda (x y z) (y x z))
+a1 a2 a3)
 
 ;;Oppgave 2
 ;;a)
