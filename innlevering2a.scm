04d8694c22468af8b88fecd5df5743cf286d8b7a
axelbaxel <axelbaxel@gmail.com>
2016-03-10 11:07:15 +0100

    Oppdatert med oppgave 2a

--- innlevering2a.scm
+++ innlevering2a.scm
@@ -6,12 +6,23 @@
 (define (p-cons x y)
   (lambda (proc) (proc x y)))
 
-(define (p-car (proc x y))
-  (x))
-(define (p-cdr (proc x y))
-  (y))
+;;(define (p-car (proc x y))
+;;  (x))
+;;(define (p-cdr (proc x y))
+;;  (y))
 ;;(p-cons "foo" "bar")
 
 ;;(p-car (p-cons "foo" "bar"))
 
-(p-cdr (p-cons "foo" "bar"))
+;;(p-cdr (p-cons "foo" "bar"))
+
+;;Oppgave 2
+;;a)
+
+(define (member? symb items)
+  (define (iter rest)
+    (cond ((null? rest) #f)
+          ((eq? (car rest) symb) #t)
+          (else (iter (cdr rest)))))
+  (iter items))
+          
